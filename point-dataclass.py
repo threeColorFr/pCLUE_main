db30853cb564f6610ba3bdd9ad41e165e833777b
threeColorFr <91378529+threeColorFr@users.noreply.github.com>
2023-02-08 17:47:21 +0800

    Update point-dataclass.py

--- point-dataclass.py
+++ point-dataclass.py
@@ -8,3 +8,9 @@ class A:
     )
 print(fields(A()))
 print(fields(A())[1].metadata)
+
+
+'''
+(Field(name='a',type=<class 'int'>,default=1,default_factory=<dataclasses._MISSING_TYPE object at 0x7f30e14cd400>,init=True,repr=True,hash=None,compare=True,metadata=mappingproxy({}),_field_type=_FIELD), Field(name='b',type=<class 'str'>,default='123',default_factory=<dataclasses._MISSING_TYPE object at 0x7f30e14cd400>,init=True,repr=True,hash=None,compare=True,metadata=mappingproxy({'help': 'this is a test'}),_field_type=_FIELD))
+{'help': 'this is a test'}
+'''
